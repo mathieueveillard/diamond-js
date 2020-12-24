ac671d89844cf670c7876e76f8aac4c6155270fe
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2020-12-24 12:02:08 +0100

    A possible solution

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,23 @@
-import { systemUnderTest } from ".";
+import { S, _, diamond } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
-  });
+it("should draw a diamond for n = 1", function () {
+  expect(diamond(1)).toEqual([[S]]);
+});
+
+it("should draw a diamond for n = 3", function () {
+  expect(diamond(3)).toEqual([
+    [_, S, _],
+    [S, S, S],
+    [_, S, _],
+  ]);
+});
+
+it("should draw a diamond for n = 5 (control)", function () {
+  expect(diamond(5)).toEqual([
+    [_, _, S, _, _],
+    [_, S, S, S, _],
+    [S, S, S, S, S],
+    [_, S, S, S, _],
+    [_, _, S, _, _],
+  ]);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,49 @@
-export function systemUnderTest(): boolean {
-  return true;
+export const S = "*";
+
+type Star = typeof S;
+
+export const _ = " ";
+
+type Space = typeof _;
+
+type Row = (Star | Space)[];
+
+type Diamond = Row[];
+
+export function diamond(dimension: number): Diamond {
+  return makeArrayOfIndexes(dimension).map(makeRow(dimension));
+}
+
+function makeRow(dimension: number) {
+  return function (line: number): Row {
+    const equivalentLine = computeEquivalentTopHalfLine(dimension)(line);
+    return makeTopHalfRow(dimension)(equivalentLine);
+  };
+}
+
+function makeTopHalfRow(dimension: number) {
+  return function (line: number): Row {
+    return makeArrayOfIndexes(dimension).map((column) => {
+      if (column < (dimension - 1) / 2 - line) {
+        return _;
+      }
+      if (column > (dimension - 1) / 2 + line) {
+        return _;
+      }
+      return S;
+    });
+  };
+}
+
+function makeArrayOfIndexes(dimension: number): number[] {
+  return [...Array(dimension)].map((_, index) => index);
+}
+
+function computeEquivalentTopHalfLine(dimension: number) {
+  return function (line: number): number {
+    if (line <= (dimension - 1) / 2) {
+      return line;
+    }
+    return dimension - 1 - line;
+  };
 }
